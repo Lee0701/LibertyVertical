6ce1d12443c25d20fee5475987bd9fa089b5ed00
Lee0701 <hgl1002@naver.com>
2021-01-20 19:15:01 +0900

    Remove font settings & alter margin directions

--- LibertyHooks.php
+++ LibertyHooks.php
@@ -20,27 +20,6 @@ class LibertyHooks extends Hooks {
 			'help-message' => 'liberty-pref-color-second-help'
 		];
 
-		$preferences['liberty-font'] = [
-			'type' => 'selectorother',
-			'label-message' => 'liberty-pref-fonts',
-			'section' => 'liberty/font',
-			'default' => 'default',
-			'options' => [
-				wfMessage( 'liberty-font-name-default' )->text() => 'default',
-				wfMessage( 'liberty-font-name-noto-sans-kr' )->text() => 'Noto Sans KR',
-				wfMessage( 'liberty-font-name-noto-serif-kr' )->text() => 'Noto Serif KR',
-				wfMessage( 'liberty-font-name-spoqa-han-sans' )->text() => 'Spoqa Han Sans',
-				wfMessage( 'liberty-font-name-nanum-gothic' )->text() => 'Nanum Gothic',
-				wfMessage( 'liberty-font-name-nanum-myeongjo' )->text() => 'Nanum Myeongjo',
-				wfMessage( 'liberty-font-name-dokdo' )->text() => 'Dokdo',
-				wfMessage( 'liberty-font-name-gaegu' )->text() => 'Gaegu',
-				wfMessage( 'liberty-font-name-hankc' )->text() => 'Hankc',
-				wfMessage( 'liberty-font-name-youth' )->text() => 'Youth',
-				wfMessage( 'liberty-font-name-malgun-gothic' )->text() => 'Malgun Gothic'
-			],
-			'help-message' => 'liberty-pref-fonts-help'
-		];
-
 		$preferences['liberty-dark'] = [
 			'type' => 'select',
 			'label-message' => 'liberty-pref-dark',
