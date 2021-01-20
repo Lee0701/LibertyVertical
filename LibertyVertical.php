3e6e8abbfe2f7e123c306f06bedd83962b8c2bb4
Lee0701 <hgl1002@naver.com>
2021-01-20 20:42:59 +0900

    Edit name strings

--- LibertyVertical.php
+++ LibertyVertical.php
@@ -1,7 +1,7 @@
 <?php // @codingStandardsIgnoreLine
 if ( function_exists( 'wfLoadSkin' ) ) {
 	wfLoadSkin( 'LibertyVertical' );
-	$wgMessagesDirs['Liberty'] = __DIR__ . '/i18n';
+	$wgMessagesDirs['LibertyVertical'] = __DIR__ . '/i18n';
 	return true;
 } else {
 	die( 'This version of the Liberty skin requires MediaWiki 1.25+' );
